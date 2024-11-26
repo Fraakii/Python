5e7f46387c162e0b42069c6310eaf92038f9d730
Fraakii <francescoperillo2703@gmail.com>
2024-11-26 13:12:16 +0100

    utente ha aggiunto gitignore

--- libreria.py
+++ libreria.py
@@ -1,14 +1,15 @@
 import random
 
-numero = random.randint(1,10)
-print(numero)
+# numero = random.randint(1,10)
+# print(numero)
+
 num1 = int(input("Inserisci un numero: "))
 num2 = random.randint(1,100)
 
 print(num1)
 print(num2)
 
-if num1 < num2:
+if num1 > num2:
     print("numero 1 maggiore")
 else:
     print("numero 2 maggiore")
